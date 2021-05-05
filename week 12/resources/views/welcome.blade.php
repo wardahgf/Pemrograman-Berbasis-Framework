35759245df085a775f2934f16170e1f6dbeda3fb
wardahgf <wardahgf88@gmail.com>
2021-05-05 23:36:57 +0700

    week 12

---
+++ week 12/resources/views/welcome.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+h1 {
+    text-align: center;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+
+    </head>
+    
+    <body>
+    
+    <h1> BIODATA  </h1>
+      <table>
+  <tr>
+    
+  </tr>
+  <tr>
+    <td>Nama :</td>
+    <td>Wardah Ghaliyah F</td>
+  </tr>
+  <tr>
+    <td>Email : </td>
+    <td>wardahgf88@gmail.com</td>
+  </tr>
+  <tr>
+    <td>Tempat, Tanggal Lahir : </td>
+    <td>Surabaya, 02-02-2000</td>
+  </tr>
+  <tr>
+    <td>Alamat : </td>
+    <td>Pasuruan, Jawa Timur </td>
+  </tr>
+  <tr>
+    <td>No Hp : </td>
+    <td>081357122952 </td>
+  </tr>
+</table>
+
+    </body>
+</html>
